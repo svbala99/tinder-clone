b2b685eeb2a359f26e461d1060c0b260f21438b3
Balamurugan V <svbala99@gmail.com>
2024-09-03 14:45:53 +0530

    chore: :sparkles: first commit from rn-cli bootstrap

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
